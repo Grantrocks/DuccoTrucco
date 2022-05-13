68c70cac96719819cd6b0cca9be86e75175f5fbf
Grant McNamara <Grantmac12345678911@outlook.com>
2022-05-13 07:29:04 -0400

    asdf

--- scripts/getstats.js
+++ scripts/getstats.js
@@ -3,13 +3,14 @@ function getbalance(){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             data=xhttp.response
-            alert(this.status)
-            alert(data)
-            alert(data.result)
-            document.getElementById("currbalance").innerHTML = `${data.message} ᕲ`;
+            if(data.result){
+                document.getElementById("currbalance").innerHTML = `${data.message} ᕲ`;
+            }else{
+                document.getElementById('currbalance').innerHTML="Error Getting Faucet Balance";
+            }
         }
     };
-    xhttp.open("GET", "filename", true);
+    xhttp.open("GET", "https://grantm.pythonanywhere.com/getfaucetbal", true);
     xhttp.responseType="json";
     xhttp.send()
 }
