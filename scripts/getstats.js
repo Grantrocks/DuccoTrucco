1c86db52e4a9e7b82fef67c002c39241de149613
Grant McNamara <Grantmac12345678911@outlook.com>
2022-05-13 07:17:36 -0400

    asdf

---
+++ scripts/getstats.js
@@ -0,0 +1,17 @@
+function getbalance(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            data=xhttp.response
+            if(data.result){
+                document.getElementById("currbalance").innerHTML = `${data.message} ᕲ`;
+            }else{
+                document.getElementById('currbalance').innerHTML="Error Getting Faucet Balance";
+            }
+        }
+    };
+    xhttp.open("GET", "filename", true);
+    xhttp.responseType="json";
+    xhttp.send()
+}
+getbalance()
