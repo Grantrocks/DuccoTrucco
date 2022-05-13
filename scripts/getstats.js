36f899b3986d88ee80618cc195289d11277bb8e7
Grant McNamara <Grantmac12345678911@outlook.com>
2022-05-13 08:00:22 -0400

    sfd

--- scripts/getstats.js
+++ scripts/getstats.js
@@ -2,6 +2,6 @@ $("button").click(function(){
     $.get("https://grantm.pythonanywhere.com/getfaucetbal", function(data, status){
         alert(status)    
         alert("HI")
-        alert("Data: " + data.message + "\nStatus: " + status);
+        alert("Data: " + data + "\nStatus: " + status);
     });
     });
